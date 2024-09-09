8a56eae3ff1511afd24a9e6c93b26a60aab91ddd
Raj Kishore S <127388780+Rajkishore08@users.noreply.github.com>
2024-09-10 02:42:03 +0530

    Update index.js

--- api/index.js
+++ api/index.js
@@ -7,23 +7,94 @@ const app = express();
 const server = http.createServer(app);
 const io = socketIO(server);
 
-app.use(express.static(path.join(__dirname, '../public')));
+const PORT = process.env.PORT || 8282;
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+const users = new Map();
+const chatRooms = new Map();
 
 io.on('connection', (socket) => {
   console.log('New user connected');
 
-  socket.on('join', (username) => {
-    console.log(`User ${username} attempting to join`);
-    socket.username = username;
-    io.emit('user joined', username);
-    socket.emit('join success', username);
+  socket.on('new user', (data, callback) => {
+    if (users.has(data.username)) {
+      callback({ success: false });
+    } else {
+      callback({ success: true });
+      socket.username = data.username;
+      socket.userColor = getRandomColor();
+      users.set(data.username, socket);
+      updateOnlineUsers();
+      socket.join('general');
+      chatRooms.set('general', (chatRooms.get('general') || []).concat(socket.username));
+      updateRoomUsers('general');
+    }
   });
 
   socket.on('disconnect', () => {
     if (socket.username) {
-      console.log(`User ${socket.username} disconnected`);
+      users.delete(socket.username);
+      updateOnlineUsers();
+      for (const [room, users] of chatRooms.entries()) {
+        const index = users.indexOf(socket.username);
+        if (index !== -1) {
+          users.splice(index, 1);
+          updateRoomUsers(room);
+        }
+      }
+    }
+  });
+
+  socket.on('chat message', (msg) => {
+    io.to(msg.room).emit('chat message', {
+      username: socket.username,
+      color: socket.userColor,
+      message: msg.message,
+      room: msg.room
+    });
+  });
+
+  socket.on('typing', (room) => {
+    socket.to(room).emit('typing', { username: socket.username, room });
+  });
+
+  socket.on('stop typing', (room) => {
+    socket.to(room).emit('stop typing', { username: socket.username, room });
+  });
+
+  socket.on('join room', (room) => {
+    socket.join(room);
+    chatRooms.set(room, (chatRooms.get(room) || []).concat(socket.username));
+    updateRoomUsers(room);
+  });
+
+  socket.on('leave room', (room) => {
+    socket.leave(room);
+    const users = chatRooms.get(room) || [];
+    const index = users.indexOf(socket.username);
+    if (index !== -1) {
+      users.splice(index, 1);
+      chatRooms.set(room, users);
+      updateRoomUsers(room);
     }
   });
 });
 
-module.exports = server;
+function updateOnlineUsers() {
+  io.emit('online users', Array.from(users.keys()));
+}
+
+function updateRoomUsers(room) {
+  io.to(room).emit('room users', { room, users: chatRooms.get(room) || [] });
+}
+
+function getRandomColor() {
+  return '#' + Math.floor(Math.random() * 0xFFFFFF).toString(16).padStart(6, '0');
+}
+
+server.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
+
+module.exports = app;
